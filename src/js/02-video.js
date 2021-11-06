50a3c9e72b4fcd9dbba4e623c1e4c9baccc52cd4
Aleksandra Maksymets <omelchenko.aleksandra@gmail.com>
2021-11-06 21:59:15 +0000

    add 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Vimeo.Player(iframe);
+
+function getPlayingTime() {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+player.on('timeupdate', throttle(getPlayingTime, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(savedTime);
